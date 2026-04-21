bef9abf3852faa28fa91cf7b03aec897517ff8f6
Mitsune <yuhu1913529212@163.com>
2026-04-21 23:31:20 +0800

    modify 优化状态机

--- 场景与代码/主程序/主体类/人物相关/人物主体.gd
+++ 场景与代码/主程序/主体类/人物相关/人物主体.gd
@@ -11,7 +11,16 @@ class_name Character_Main
 @export var attack_bullet:PackedScene
 @export var attack_effect:PackedScene
 
-enum State{常态,移动,技能1,必杀1,冲刺,死亡}
+enum State{NONE,常态,移动,技能1,必杀1,冲刺,死亡}
+var current_state: State = State.常态 :
+	set(v):
+		print(current_state)
+		if current_state == v: return
+		exit_state(current_state)
+		transition_state(current_state,v)
+		current_state=v
+		enter_state(v)
+
 var team:String
 var gravity:=ProjectSettings.get("physics/2d/default_gravity") as float
 var move_speed:int
@@ -25,10 +34,6 @@ var attack_timer = Timer.new()
 var skill_timer = Timer.new()
 var is_attack_timer_timeout:bool=false
 var is_skill_timer_timeout:bool=true
-var current_state:int=-1:
-	set(v):
-		transition_state(current_state,v)
-		current_state=v
 
 var move_left:String="move_left_1p"
 var move_right:String="move_right_1p"
@@ -42,15 +47,13 @@ var ultimate:String="ultimate_1p"
 ##初始化函数
 func _ready() -> void:
 	await character.ready                          #等人物先加载
+	team=character_data.team                       #队伍
 	move_speed=character.move_speed                #移速
 	acceleration=character.acceleration            #加速度
 	friction=character.friction                    #减速度
 	direction=character_data.direction             #朝向
 	character_data.direction_changed.connect(update_direction)
 	update_direction(direction)
-	current_state=0                                #初始状态值
-	team=character_data.team                       #队伍
-	is_alive=true                                  #是否活着
 	#普攻和技能cd计时器
 	attack_timer.wait_time = character.attack_interval
 	attack_timer.timeout.connect(_on_attack_timer_timeout)
@@ -73,11 +76,9 @@ func _ready() -> void:
 
 ##每帧效果函数
 func _physics_process(delta: float) -> void:
-	while true:
-		var next_state:=get_next_state(current_state) as int
-		if next_state==current_state:
-			break
-		current_state=next_state
+	var next_state = get_next_state(current_state)
+	if next_state != current_state:
+		current_state = next_state
 	tick_physics(current_state,delta)
 	if character_ctrler.is_gravity:
 		current_velocity.y += gravity * delta
@@ -85,83 +86,36 @@ func _physics_process(delta: float) -> void:
 	if is_allow_key_move and not is_dead() and not character_ctrler.get_is_moving():
 		move(move_speed,delta)
 
-
-##惯性移动函数
-func move(max_speed:float,delta):
-	var input_dir = Input.get_vector(move_left,move_right,move_up,move_down)
-	var target_direction = input_dir.normalized()
-	if input_dir != Vector2.ZERO:
-		if current_velocity.length() > 0.01:
-			var dot = current_velocity.normalized().dot(target_direction)
-			if dot < 0:  # 按方向相反的键急停（夹角大于90度）
-				current_velocity = Vector2.ZERO
-		var target_velocity = target_direction*max_speed
-		current_velocity = current_velocity.move_toward(target_velocity, acceleration * delta)
-	else:
-		current_velocity = current_velocity.move_toward(Vector2.ZERO, friction * delta)
-	character.velocity = current_velocity
-	character.move_and_slide()
-
-
 ##状态每帧的效果函数
-func tick_physics(state:State,delta: float) -> void:
+func tick_physics(state:State,_delta: float) -> void:
 	match  state:
-		State.常态,State.移动:
-			if Input.is_action_pressed(attack):
-				if attack_timer.is_stopped():
-					attack_timer.start() # 如果按住且计时器未运行，则启动
-					character_ctrler.shoot(attack_bullet,Vector2(50*direction,0))
-				elif is_attack_timer_timeout:
-					is_attack_timer_timeout=false
-					character_ctrler.shoot(attack_bullet,Vector2(50*direction,0))
-			else:
-				if not attack_timer.is_stopped() and is_attack_timer_timeout:
-					is_attack_timer_timeout=false
-					attack_timer.stop() # 松开按键且计时归零，停止计时器
+		State.常态:
+			fire_bullet()
+		State.移动:
+			move_animation()
+			fire_bullet()
 		State.冲刺,State.技能1,State.必杀1:
 			pass
 		State.死亡:
 			character_ctrler.set_invincible(true)
-			#character_ctrler.stop_move()
 			character_ctrler.apply_gravity(true)
-			character.velocity.y += gravity * delta
 			is_alive=false
-
 
 ##下一个状态逻辑函数
 func get_next_state(state:State)->State:
 	if is_dead():
 			return State.死亡
+	if state in [State.常态, State.移动]:
+		var common = idle_move()
+		if common != State.NONE:
+			return common
 	match state:
 		State.常态:
 			if Input.is_action_pressed(move_left) or Input.is_action_pressed(move_right):
 				return State.移动
-			if Input.is_action_just_pressed(skill) and is_skill_timer_timeout:
-				if skill_timer.is_stopped():
-					skill_timer.start()
-				is_skill_timer_timeout=false
-				return State.技能1
-			if Input.is_action_just_pressed(ultimate) and character_data.mp>=100:
-				character_data.mp-=100
-				return State.必杀1
-			if Input.is_action_just_pressed(dash) and character_data.energy>=25:
-				character_data.energy-=25
-				return State.冲刺
 		State.移动:
-			transition_state(State.常态, State.移动)
 			if not Input.is_action_pressed(move_left) and not Input.is_action_pressed(move_right):
 				return State.常态
-			if Input.is_action_just_pressed(skill) and is_skill_timer_timeout:
-				if skill_timer.is_stopped():
-					skill_timer.start()
-				is_skill_timer_timeout=false
-				return State.技能1
-			if Input.is_action_just_pressed(ultimate) and character_data.mp>=100:
-				character_data.mp-=100
-				return State.必杀1
-			if Input.is_action_just_pressed(dash) and character_data.energy>=25:
-				character_data.energy-=25
-				return State.冲刺
 		State.冲刺:
 			if not anplayer.is_playing():
 				return State.常态
@@ -171,42 +125,78 @@ func get_next_state(state:State)->State:
 		State.必杀1:
 			if not anplayer.is_playing():
 				return State.常态
-		
 	return state
 
+##状态进入时
+func enter_state(state:State):
+	match state:
+		pass
+
+##状态退出时
+func exit_state(state:State):
+	match state:
+		pass
 
 ##状态动画播放函数
 func transition_state(_from:State,to:State) -> void:
-	var dir:=Input.get_axis(move_left,move_right)
 	match to:
 		State.常态:
 			an_paly("常态")
 		State.移动:
-			if dir*direction>0:
-				an_paly("前进")
-			else:
-				an_paly("后退")
+			move_animation()
 		State.技能1:
 			an_paly("技能1")
 		State.必杀1:
 			an_paly("必杀1")
 		State.冲刺:
-			if Input.is_action_pressed(move_left):
-				an_paly("冲刺后")
-			else:
-				an_paly("冲刺前")
+			dash_animation()
 		State.死亡:
 			an_paly("死亡")
 
+##移动动画播放函数
+func move_animation():
+	var dir:=Input.get_axis(move_left,move_right)
+	if dir*direction>0:
+		an_paly("前进")
+	else:
+		an_paly("后退")
+
+##冲刺动画播放函数
+func dash_animation():
+	var dir:=Input.get_axis(move_left,move_right)
+	if dir*direction<0:
+		if anplayer.has_animation("冲刺后"):
+			an_paly("冲刺后")
+		else:
+			an_paly("冲刺")
+	else:
+		if anplayer.has_animation("冲刺前"):
+			an_paly("冲刺前")
+		else:
+			an_paly("冲刺")
 
 ##动画播放函数
 func an_paly(an_name:String):
 	if anplayer.has_animation(an_name):
 		anplayer.play(an_name)
 	else:
-		print("未找到 %s 动画"%[an_name])
+		printerr("缺失动画: ", an_name)
 		return
 
+##常态，移动状态通用效果
+func idle_move() -> State:
+	if Input.is_action_just_pressed(skill) and is_skill_timer_timeout:
+		if skill_timer.is_stopped():
+			skill_timer.start()
+		is_skill_timer_timeout=false
+		return State.技能1
+	if Input.is_action_just_pressed(ultimate) and character_data.mp>=100:
+		character_data.mp-=100
+		return State.必杀1
+	if Input.is_action_just_pressed(dash) and character_data.energy>=25:
+		character_data.energy-=25
+		return State.冲刺
+	return State.NONE
 
 ##受击处理函数
 func _on_hurtbox_hurt(hitbox: Variant, attack_data: AttackData) -> void:
@@ -229,21 +219,50 @@ func _on_hurtbox_hurt(hitbox: Variant, attack_data: AttackData) -> void:
 		attack_effect_node.set_attack_effect(attack_type,hitbox.global_position)
 	attack_effect_node.set_hitstop(hitstop)
 	
+##惯性移动函数
+func move(max_speed:float,delta):
+	var input_dir = Input.get_vector(move_left,move_right,move_up,move_down)
+	var target_direction = input_dir.normalized()
+	if input_dir != Vector2.ZERO:
+		if current_velocity.length() > 0.01:
+			var dot = current_velocity.normalized().dot(target_direction)
+			if dot < 0:  # 按方向相反的键急停（夹角大于90度）
+				current_velocity = Vector2.ZERO
+		var target_velocity = target_direction*max_speed
+		current_velocity = current_velocity.move_toward(target_velocity, acceleration * delta)
+	else:
+		current_velocity = current_velocity.move_toward(Vector2.ZERO, friction * delta)
+	character.velocity = current_velocity
+	character.move_and_slide()
 
-
+##更新朝向
 func update_direction(direct:float):
 	direction=direct
 	character.scale.x=direction
-	#print("当前朝向：",direction)
 
+##判断是否死亡
 func is_dead():
 	if character_data.hp<=0 or not is_alive:
 		return true
 
+##普攻弹幕发射
+func fire_bullet():
+	if Input.is_action_pressed(attack):
+		if attack_timer.is_stopped():
+			attack_timer.start()
+			character_ctrler.shoot(attack_bullet,Vector2(50*direction,0))
+		elif is_attack_timer_timeout:
+			is_attack_timer_timeout=false
+			character_ctrler.shoot(attack_bullet,Vector2(50*direction,0))
+	else:
+		if not attack_timer.is_stopped() and is_attack_timer_timeout:
+			is_attack_timer_timeout=false
+			attack_timer.stop()
+
 ##计时结束的回调函数
 func _on_attack_timer_timeout():
 	is_attack_timer_timeout=true;
+
 func _on_skill_timer_timeout():
 	is_skill_timer_timeout=true;
 	skill_timer.stop()
-	print("cd结束")
