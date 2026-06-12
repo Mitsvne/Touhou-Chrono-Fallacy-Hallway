7893ad00b82ec82421238903a8d0a97878b82f75
Mitsune <yuhu1913529212@163.com>
2026-06-12 23:33:31 +0800

    modify 机制修改：常规弹幕不再碰撞抵消

--- 场景与代码/人物/灵梦/弹幕/灵梦技能1弹幕.gd
+++ 场景与代码/人物/灵梦/弹幕/灵梦技能1弹幕.gd
@@ -8,11 +8,9 @@ extends Area2D
 @onready var bullet_ctrler: Bullet_Ctrler = $class/Bullet_Ctrler
 @onready var effect_ctrler: Effect_Ctrler = $class/Effect_Ctrler
 @onready var hitbox: Hitbox = $Hitbox
-@onready var hurtbox: Hurtbox = $Hurtbox
 
 func _ready():
 	self.area_entered.connect(_on_area_entered)
-	hurtbox.hurt.connect(_on_hurtbox_hurt)
 	await get_tree().process_frame
 	AudioManager.play_sfx(add_audio,-6)
 	var target=bullet_ctrler.get_target().global_position
@@ -38,20 +36,7 @@ func _on_area_entered(area: Area2D) -> void:
 		bullet_data.bullet_owner.character_data.mp+=10
 		bullet_ctrler.stop_move()
 		bullet_ctrler.disable_box(hitbox,true)
-		bullet_ctrler.disable_box(hurtbox,true)
 		an.play(&"hit")
 		AudioManager.play_sfx(hit_audio)
 		await an.animation_finished
 		queue_free()
-
-func _on_hurtbox_hurt(box: Hitbox, attack_data: AttackData) -> void:
-	if box.owner.is_in_group("bullets"):
-		bullet_data.hp-=attack_data.damage
-		if bullet_data.hp<=0:
-			bullet_ctrler.stop_move()
-			bullet_ctrler.disable_box(hitbox,true)
-			bullet_ctrler.disable_box(hurtbox,true)
-			an.play(&"hit")
-			AudioManager.play_sfx(hit_audio)
-			await an.animation_finished
-			queue_free()
